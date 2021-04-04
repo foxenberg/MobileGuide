53f5bc9a9ff8549f583b5dbdd913bde987484433
foxenberg <daniel.korshunov.2001@gmail.com>
2021-04-05 00:37:10 +0300

    Added widget ClipPath on page selected routes or places

--- mobile_guid/lib/screens/secondScreen.dart
+++ mobile_guid/lib/screens/secondScreen.dart
@@ -1,38 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_guid/widgets/customClipper.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({Key key}) : super(key: key);
 
   @override
+  
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 1,
-            child: Container(
-              color: Colors.lightBlue,
-              child: Center(
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text('Маршруты'),
+      backgroundColor: Colors.grey,
+      body: Stack(        
+          children: <Widget>[
+            ClipPath(
+              clipper: MyCustomClipper(),
+              child: Container(
+               color: Colors.pink,
                 ),
               ),
-            ),
-          ),
-          Flexible(
-              flex: 1,
-              child: Container(
-                color: Colors.lightGreen,
-                child: Center(
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text('Места'),
-                  ),
+              InkWell(
+                onTap: () {},
+                              child: Container(
+                  margin: EdgeInsets.all(5),
+                  alignment: Alignment.bottomLeft,
+                  child: Image.asset("assets/images/routes.png")),
+              ),
+              ClipPath(
+                clipper: MyClipper(),
+                child: Container(
+                  color: Colors.green,)
                 ),
-              )),
-        ],
+                InkWell(
+                  onTap: (){},
+                                  child: Container(
+                    margin: EdgeInsets.all(5),
+                  alignment: Alignment.topRight,
+                  child: Image.asset("assets/images/routes.png"),
+                  ),
+                )
+              
+          ],
+          
       ),
     );
   }
 }
+
+
---
+++ mobile_guid/lib/widgets/customClipper.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyCustomClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    
+
+    path.close();
+    return path;
+  }
+
+  @override 
+  bool shouldReclip(CustomClipper<Path> oldClipper){
+    return false;
+  }
+}
+
+class MyClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    
+
+    path.close();
+    return path;
+  }
+
+  @override 
+  bool shouldReclip(CustomClipper<Path> oldClipper){
+    return false;
+  }
+}
